403f0b77ae8530d735ce875e2c28981d3cbe869d
Arvind Sharma <mail.arvind.s@gmail.com>
2021-11-09 11:30:59 +0000

    random string generator function added to express_server.js

--- express_server.js
+++ express_server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 const PORT = 3000; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs") 
+
+function generateRandomString() {
+  let randomString = [];
+  for (let i = 0; i < 6; i++) {
+    let rString = Math.random(String(64));
+    randomString.push(rString[i]);
+  }
+  return String(randomString);
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -35,6 +46,10 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
